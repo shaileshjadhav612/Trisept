1005197f37dbe138087a0695a0a7af26d317e3a5
shaileshjadhav612 <shaileshjadhav612@gmail.com>
2020-03-18 13:23:19 +0530

    Cot

---
+++ splitString/src/main/java/splitString/splitString/App.java
@@ -0,0 +1,13 @@
+package splitString.splitString;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
